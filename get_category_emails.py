6a6d1f2e816859b5df5f35856ecb00fbfc24d4d7
paulazoo <pkzr3@k12albemarle.org>
2020-03-11 00:39:26 -0400

    by category go through each category value

--- get_category_emails.py
+++ get_category_emails.py
@@ -19,14 +19,21 @@ desired=config.desired
 category = input("Do groups by which category?")
 #get relevant category column
 category_Summary=All_Summary[category]
-#relevant category value
-category_val='History'
-#index of correct category values in category column
-category_yes=category_Summary[category_Summary==category_val].index
-category_yes=pd.Index.tolist(category_yes)
-# print(category_yes)
 
-#get specific emails that are correct e.g. 'History' in 'Major' column emails
-specific_emails = [allEmailsNoDuplicates[i] for i in category_yes]
-# print(specific_emails)
+#%%
+
+#go through each category value
+category_strs=category_Summary.drop_duplicates()
+#relevant category value
+for category_val in category_strs:
+    # print(major)
+    #index of correct category values in category column
+    category_yes=category_Summary[category_Summary==category_val].index
+    category_yes=pd.Index.tolist(category_yes)
+    # print(category_yes)
+    
+    #get specific emails that are correct e.g. 'History' in 'Major' column emails
+    specific_emails = [allEmailsNoDuplicates[i] for i in category_yes]
+    # print(specific_emails)
+    
 #%%
