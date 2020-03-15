d7ebe7b4ce1849097957eedc4234f30b0a0b1d98
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-03-15 09:25:46 -0400

    made it loop through the number of calls

--- working_main_creates_groups.py
+++ working_main_creates_groups.py
@@ -36,7 +36,7 @@ print(str(len(allGeneratedGroups[0][0])))
 #login file
 import logging_in
 #WaitTime is used with time.sleep() to make sure webpages are loading.
-waitTime1 = 2
+waitTime1 = 1
 #login using the login function in logging_in.py
 web=logging_in.login(waitTime1)
 
@@ -58,6 +58,7 @@ web.driver.switch_to.default_content()
 #make the number of generatedGroups corresponding to the number of calls
 for generatedGroups in allGeneratedGroups:
     #start creating the hangout for each group in the generatedGroups for each designated call
+    groupNum = 1
     for subGroup in generatedGroups:
         #groupName using the date and groupNum
         #groupName = dateNow + " Call " + str(groupNum) + " Key: "
@@ -123,7 +124,7 @@ for generatedGroups in allGeneratedGroups:
             #get out of iframe for making groups
             web.driver.switch_to.default_content()
             #type and enter group introduction messages
-            time.sleep(waitTime1)
+            time.sleep(waitTime1*2)
             web.type("Hello! Welcome to the group for " + str(groupName) + ". For this group, please make this the " + str(callNum) + " call. At the designated start time, someone should initiate the call. In order for this to work as smoothly as possible, we need to coordinate our calling. Albert originally planned for each call to be 15 minutes, but times will be flexible depending on how ya'll like the lengths, so please check the GroupMe for the official lengths for each call! You can always return to this chat later if ya'll want to talk more :) . Additionally, if you would like to leave early, just leave the groups that you won't be able to call in. So, you could choose to only partake in calls 1 to 3 if you prefer, but we all would love if you join all the calls. :D Thanks for helping make this happen!")
             #web.type("Hello! This is the testing for a program. Please ignore this hangout. You may exit.")
             time.sleep(waitTime1)
@@ -147,5 +148,6 @@ for generatedGroups in allGeneratedGroups:
         #finished!
         print("The " + str(groupNum) + " group of the " + str(callNum) + " call has worked! We got to the end of the line!")
         groupNum += 1
+    callNum += 1
     
     
