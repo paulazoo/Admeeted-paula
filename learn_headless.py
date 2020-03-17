b21bdde1fda428033a675eb29f50803adb48f5b1
paulazoo <pkzr3@k12albemarle.org>
2020-03-16 20:52:11 -0400

    headless is stpd. faster login tho

--- learn_headless.py
+++ learn_headless.py
@@ -1,7 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 16 19:53:19 2020
+#%%
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-@author: pkzr3
-"""
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+web = webdriver.Chrome(options=options)
+#%%
+web.get('https://accounts.google.com/signin/v2/identifier?service=talk&passive=1209600&continue=https%3A%2F%2Fhangouts.google.com%2Fwebchat%2Fstart&followup=https%3A%2F%2Fhangouts.google.com%2Fwebchat%2Fstart&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+#sign in email
+web.find_element_by_css_selector("input[type='email']").send_keys('VirtualVisitas2.0')
+web.find_element_by_css_selector("input[type='email']").send_keys(Keys.RETURN)
+
+web.find_element_by_css_selector("input[type='password']").send_keys('Apaar&AlbertSal')
+web.find_element_by_css_selector("input[type='password']").send_keys(Keys.RETURN)
+web.get_screenshot_as_file('main-page.png')
+
+
 
--- logging_in.py
+++ logging_in.py
@@ -1,20 +1,19 @@
 from webbot import Browser
 import time
-import selenium
+from selenium import webdriver
 
+#%%
 #starting browser and logging in to hangouts
 def login(waitTime1):    
     #start new browser
     web = Browser()
-    time.sleep(waitTime1*2)
     #go to hangouts
     web.driver.get('https://accounts.google.com/signin/v2/identifier?service=talk&passive=1209600&continue=https%3A%2F%2Fhangouts.google.com%2Fwebchat%2Fstart&followup=https%3A%2F%2Fhangouts.google.com%2Fwebchat%2Fstart&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-    #sign in email
-    web.type('VirtualVisitas2.0' , into='Email')
-    web.click('NEXT' , tag='span')
-    #sign in password
     time.sleep(waitTime1)
-    web.type('Apaar&AlbertSal' , into='Password' , id='passwordFieldId')
-    web.click('NEXT' , tag='span') # you are logged in . woohoooo
-    web.click('confirm')
+    #sign in email
+    web.driver.find_element_by_css_selector("input[type='email']").send_keys('VirtualVisitas2.0')
+    web.driver.find_element_by_css_selector("input[type='email']").send_keys(Keys.RETURN)
+    time.sleep(waitTime1)
+    web.driver.find_element_by_css_selector("input[type='password']").send_keys('Apaar&AlbertSal')
+    web.driver.find_element_by_css_selector("input[type='password']").send_keys(Keys.RETURN)
     return web
