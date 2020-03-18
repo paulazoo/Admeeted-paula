c358aa817f0eddb7a5ae9b83efb7d675108e5c4b
paulazoo <pkzr3@k12albemarle.org>
2020-03-18 19:56:43 -0400

    config excelparser class

---
+++ config.py
@@ -0,0 +1,61 @@
+#import pandas package for doing stuff with data
+import pandas as pd
+import os
+
+class ExcelParser():
+    #define an initial function that when run, initializes variables
+    def __init__(self, excel_name):
+        #get current directory path on specific computer using os    
+        dir_path = os.path.dirname(os.path.realpath(excel_name))
+        print(dir_path)
+        #file location for respones excel file
+        excel_file = dir_path + "\\" + excel_name
+        #all_summary holds the survey response data as a pandas dataframe
+        all_summary = pd.read_excel(excel_file)
+        #remove all rows (people cases) with duplicate emails except last entered row by that email person
+        all_summary=all_summary.drop_duplicates(subset=['Email Address'], keep='last').reset_index(drop=True)
+        #remove all rows (people cases) with duplicate NAMES except last entered row by that name person
+        all_summary=all_summary.drop_duplicates(subset=['Full Name'], keep='last').reset_index(drop=True)
+        #Get indices for non gmails
+        non_gmails_indices = all_summary[all_summary['Email Address'].str.endswith("@gmail.com") == False].index
+        dropped_ppl=[all_summary["Full Name"][i] for i in list(non_gmails_indices)]
+        all_summary=all_summary.drop(non_gmails_indices).reset_index(drop=True)
+        
+        #Get indices for non participating (CANT call at the time). Removes either "No"
+        non_participating_indices = all_summary[all_summary['Participating'].str.endswith("Yes") == False].index
+        print(non_participating_indices)
+        dropped_ppl=dropped_ppl + [all_summary["Full Name"][i] for i in list(non_participating_indices)]
+        all_summary=all_summary.drop(non_participating_indices).reset_index(drop=True)
+    
+        #Removes null or no response in the participating  question
+        null_participating_indices = all_summary[all_summary['Participating'].isnull()].index
+        print(null_participating_indices)
+        dropped_ppl=dropped_ppl + [all_summary["Full Name"][i] for i in list(null_participating_indices)]
+        all_summary=all_summary.drop(null_participating_indices).reset_index(drop=True)
+        
+        #deletes emails that are too long (greater than 300 chars) because they would slow down the program and no legit emails are longer than 300 chars
+        too_long_indices = all_summary[all_summary['Email Address'].str.len() > 300].index  
+        dropped_ppl=dropped_ppl + [all_summary["Full Name"][i] for i in list(too_long_indices)]
+        all_summary=all_summary.drop(too_long_indices).reset_index(drop=True)
+        
+        #print ppl whos emails didn't make it
+        #print(all_summary['Participating'])
+        #write the full names of dropped gmails into a file
+        with open("dropped_ppl.txt", "w") as outfile:
+            outfile.write("\n".join(str(dropped_ppl)))
+        
+        self.all_summary = all_summary
+
+        #all_emails holds the final list of emails w no duplicate emails
+        self.all_emails=list(all_summary["Email Address"])
+        
+        #desired for the desired number of ppl per group
+        self.desired = int(input("How many people would you like in a group? "))
+        
+        #number of total calls
+        self.numCalls = int(input("How many times do you want people to call? "))
+
+        #number of total threads
+        self.numThreads = int(input("How many threads/tabs/windows do you want to use? More means the program runs faster but takes more memory. "))
+        
+        
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#This was based on the main_v10_threading.py file. 
 #some packages to import
 #to install a package on python: pip install [package name]
 from webbot import Browser
@@ -12,39 +11,36 @@ import hangout_tools
 #%%
 #initialize variables
 #the config file cleans the data and gets the starting variable values
-import config_all as config
-config.init('Testers Virtual Visitas (Responses).xlsx')
-#get variables from config variables
-All_Summary=config.All_Summary
-desired=config.desired
-allEmails=config.allEmails
+from config import ExcelParser
 
+myparser = ExcelParser('Testers Virtual Visitas (Responses).xlsx')
+
+myparser.all_summary
+myparser.desired
+myparser.all_emails
+myparser.numCalls
+myparser.numThreads
 #%%
 #group manipulations
 import groups
 category = input("category? ")
+#if category doesn't equal random
 if category != "Random":
-    by_category, category_strs=groups.get_category_random(All_Summary, allEmails, category)
-
-#%%
-numCalls = int(input("How many times do you want people to call? "))
-
-numThreads = int(input("How many threads/tabs/windows do you want to use? More means the program runs faster but takes more memory. "))
-
-#%%
+    by_category, category_strs=groups.get_category_random(myparser.all_summary, myparser.all_emails, category)
+    
 #create groups using the createGroups function defined in groups.py file
 allGeneratedGroups = []
 
-#allGeneratedGroups = allGeneratedGroups + groups.createGroups(allEmails, desired, 1)
+allGeneratedGroups = allGeneratedGroups + groups.createGroups(myparser.all_emails, myparser.desired, 1)
 
 for major in by_category:
-    allGeneratedGroups = allGeneratedGroups + groups.createGroups(major, desired, 2)
+    allGeneratedGroups = allGeneratedGroups + groups.createGroups(major, myparser.desired, 2)
     print(by_category.index(major))
-#%%
+
 print(allGeneratedGroups)
 #%%
-for i in range(1, numCalls+1):
-    allGeneratedGroups = allGeneratedGroups + groups.createGroups(allEmails, desired, i)
+for i in range(1, myparser.numCalls+1):
+    allGeneratedGroups = allGeneratedGroups + groups.createGroups(myparser.all_emails, myparser.desired, i)
     print("hi")
 #print the generated groups to check
 print(len(allGeneratedGroups[0]))
@@ -52,17 +48,14 @@ print(allGeneratedGroups)
 
 #%%
 import helpers
-
-#%%
 import threading
 # creating thread 
 catchErrors = True
 
-batchedLists = helpers.splitList(allGeneratedGroups, numThreads)
+batchedLists = helpers.splitList(allGeneratedGroups, myparser.numThreads)
 #batchedLists = splitList(batchedLists, numThreads)
 print("Batched lists are:")
 print(batchedLists)
-
 
 #%%
 thread_list=[]
