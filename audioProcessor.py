54de332a7662bc3d0e3e4e17c6c2f435a821b625
nlist2 <noah2list@gmail.com>
2020-03-20 10:56:24 -0500

    plot

--- audioProcessor.py
+++ audioProcessor.py
@@ -12,7 +12,6 @@ from IPython.display import Audio
 from numpy.fft import fft, ifft
 
 
-
 #the following records audio from your microphone and saves it as a .WAV file
 fs = 44100  # Sample rate
 seconds = 3  # Duration of recording
@@ -21,3 +20,8 @@ myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
 write('output.wav', fs, myrecording)
 
+Fs, data = read("output.wav")
+data = data[:,0]
+plt.figure()
+plt.plot(data)
+plt.show()
