675ca10f4a66835b45933a5bf036face53560708
nlist2 <noah2list@gmail.com>
2020-03-20 10:44:35 -0500

    audioprocess

--- audioProcessor.py
+++ audioProcessor.py
@@ -3,3 +3,15 @@
 #set up "bot" in the Google Hangout... record the audio
 
 #process what the data means? how much silence? quiet (low dB?)
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+#the following records audio from your microphone and saves it as a .WAV file
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)
+
