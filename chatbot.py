cb81d395c0d521844f8701e8ffecbe1550c3b83e
nlist2 <noah2list@gmail.com>
2020-03-21 10:39:37 -0500

    upd

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 def chatbot():
     import random
+    import pandas
 
     minutes = randint(2,4)
     time.sleep(minutes) #do the following every 2 to 4 minutes (random)
@@ -8,6 +9,10 @@ def chatbot():
     value = randint(1,3)
     if(value == 1):
         #chat an icebreaker
+        icebreaker_data = pd.read_csv("random_questions.csv", encoding="ISO-8859-1")
+        icebreaker = list(icebreaker_data['Question'])
+        question_number = randint(0, len(icebreaker))
+        #chat icebreaker[question_number]
     elif(value == 2):
         #chat a trivia question
     else:
