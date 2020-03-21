2042386b54a78517188213bb08ce659233329107
alzh9000 <32472855+alzh9000@users.noreply.github.com>
2020-03-21 11:33:47 -0400

    successfully ran Stanford and MIT. Important thing for no errors is to use less threads, I think.

--- main_not_pandas.py
+++ main_not_pandas.py
@@ -34,7 +34,7 @@ logging.warning('Log started with user: '+current_user)
 #the config file cleans the data and gets the starting variable values
 from config import ExcelParser
 
-myparser = ExcelParser("Coke Scholars Virtual Visitas (Responses).xlsx")
+myparser = ExcelParser("Tree'ing for Quaran-teens (Responses).xlsx")
 
 myparser.all_summary
 myparser.desired
