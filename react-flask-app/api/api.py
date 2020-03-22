2b3e2e17f7a9d659712e3b41446bd97847d77b3b
paulazoo <pkzr3@k12albemarle.org>
2020-03-22 08:10:53 -0400

    uhhh playing w flask?

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route('/time')
 def get_current_time():
     return jsonify({'time': time.time()})
+
+@app.route('/hitest')
+def hi():
+    return "<h1>Hiiiii!!!</h1>"
+            
+
