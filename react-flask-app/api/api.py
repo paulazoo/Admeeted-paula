029af741b0b41e79a2e51ed3551df1c89eaaae54
sammie7242 <sammie7242@gmail.com>
2020-03-26 19:58:30 -0400

    Google Authentication Login on SQLite

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,14 +1,14 @@
-import time
-from flask import Flask, jsonify
+# import time
+# from flask import Flask, jsonify
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return jsonify({'time': time.time()})
+# @app.route('/time')
+# def get_current_time():
+#     return jsonify({'time': time.time()})
 
-@app.route('/hitest')
-def hi():
-    return "<h1>Hiiiii!!!</h1>"
+# @app.route('/hitest')
+# def hi():
+#     return "<h1>Hiiiii!!!</h1>"
             
 
